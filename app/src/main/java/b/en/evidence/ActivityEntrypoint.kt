86a10f9be0408635dc2c0b73f7523fb191739496
Ben Foster <fosterben150@gmail.com>
2020-06-25 03:44:23 +0100

    fackin' calars cant

--- app/src/main/java/b/en/evidence/ActivityEntrypoint.kt
+++ app/src/main/java/b/en/evidence/ActivityEntrypoint.kt
@@ -7,18 +7,21 @@ import android.content.Context
 import android.content.DialogInterface
 import android.content.pm.PackageManager
 import android.content.pm.PermissionInfo
+import android.content.res.Resources
 import android.hardware.biometrics.BiometricManager
 import android.hardware.biometrics.BiometricPrompt
 import android.os.Bundle
 import android.os.CancellationSignal
+import android.text.TextWatcher
+import android.view.KeyEvent
 import android.view.animation.Animation
-import android.widget.Button
-import android.widget.ProgressBar
-import android.widget.Toast
+import android.view.inputmethod.InputMethodManager
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.app.DialogCompat
 import androidx.core.content.ContextCompat
+import androidx.core.widget.addTextChangedListener
 import b.en.evidence.auth.AuthEntrypoint
 import java.util.concurrent.Executor
 import java.util.jar.Manifest
@@ -80,6 +83,37 @@ class ActivityEntrypoint : AppCompatActivity() {
             }
         } else {
             setContentView(R.layout.activity_set_auth)
+            val pass = findViewById<EditText>(R.id.setAuthPassword)
+            val cPass =findViewById<EditText>(R.id.setAuthPasswordConfirm)
+            val passL =findViewById<TextView>(R.id.setAuthPassLength)
+            passL.text = String.format(resources.getString(R.string.password_count), pass.text.length)
+            pass.addTextChangedListener {
+                val l = pass.text.length
+                if (l > 7) passL.setTextColor(resources.getColor(R.color.colorPrimaryDark, null))
+                else if (l > 3) passL.setTextColor(resources.getColor(R.color.colorPrimary, null))
+                else passL.setTextColor(resources.getColor(R.color.colorAccent, null))
+                passL.text = String.format(resources.getString(R.string.password_count), l)
+            }
+            pass.setOnKeyListener { /*view*/ _, i, keyEvent ->
+                if (keyEvent.action == KeyEvent.ACTION_DOWN && i == KeyEvent.KEYCODE_ENTER) {
+                    if (cPass.text.isBlank()) {
+                        cPass.requestFocus()
+                        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                        imm.showSoftInput(cPass, InputMethodManager.SHOW_IMPLICIT)
+                    }
+                    return@setOnKeyListener true
+                }
+                return@setOnKeyListener false
+            }
+            findViewById<Button>(R.id.setAuthNext).setOnClickListener {
+                if (pass.text.isBlank() || cPass.text.isBlank() ) {
+                    showToast("Nothing in either field")
+                } else if (pass.text.length < 8) {
+                    showToast("Not long enough")
+                } else if (pass.text != cPass.text) {
+                    showToast("Don't match")
+                }
+            }
         }
 //        if (ContextCompat.checkSelfPermission(this, "USE_BIOMETRICS") == PackageManager.PERMISSION_GRANTED) {
 //            val bio = BiometricPrompt.Builder(this)
